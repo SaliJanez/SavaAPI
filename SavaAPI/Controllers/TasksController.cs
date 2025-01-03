d6ba224cb75223533656ee49f3a649dec8baa402
SaliJanez <janez.sali@gmail.com>
2025-01-03 15:55:31 +0100

    Initial commit
    Task Management API with basic CRUD operations and in-memory database

---
+++ SavaAPI/Controllers/TasksController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SavaAPI.Data;
+
+namespace SavaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tasks>>> GetTask()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tasks>> GetTask(Guid id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(Guid id, Tasks tasks)
+        {
+            if (id != tasks.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tasks).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public async Task<ActionResult<Tasks>> PostTask(Tasks tasks)
+        {
+            _context.Tasks.Add(tasks);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTask", new { id = tasks.Id }, tasks);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(Guid id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskExists(Guid id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SavaAPI/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SavaAPI.Data
+{ 
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+    }
+}
